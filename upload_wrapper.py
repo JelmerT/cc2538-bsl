1d8d1a01109454a8b19ac6701f07853face456d3
mateuskahler <mateus.kahler@gmail.com>
2021-07-23 18:29:37 -0300

    retry interval

--- upload_wrapper.py
+++ upload_wrapper.py
@@ -3,12 +3,14 @@
 import sys
 from typing import List
 import logging
+import time
 
 from cc2538 import flash_main
 
 
 def main(args: List[str]):
     MAX_RETRIES = 3
+    RETRY_INTERVAL_S = 2
     success = False
 
     for _ in range(MAX_RETRIES):
@@ -16,6 +18,7 @@ def main(args: List[str]):
             flash_main(args)
         except BaseException as error:
             print(f'{error}')
+            time.sleep(RETRY_INTERVAL_S)
         else:
             success = True
             break
